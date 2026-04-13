e11ba026ec24c626a8a06a9f9eaa88e694361ba8
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-04-13 12:56:47 +0100

    Add new card entries to cards.js

--- cards.js
+++ cards.js
@@ -1,40 +1,85 @@
 const cards = [
-    {
-    "id": "card_47",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0021492_2071.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/ADV0000544_2071.jpg",
+  {
+    "id": "card_81",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0021577_1148.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi139963.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/100036/abelia-grandiflora/details",
-    "back_text": "Abelia × grandiflora\n\n(glossy abelia)"
+    "rhs_url": "https://www.rhs.org.uk/plants/10041/lewisia-cotyledon/details",
+    "back_text": "Lewisia cotyledon\n\n(Siskiyou lewisia)"
   },
   {
-    "id": "card_48",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035452_4309.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi116754.jpg",
+    "id": "card_96",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHDE18096_1572.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa16092.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/155/acanthus-mollis/details",
-    "back_text": "Acanthus mollis\n\n(bear's breech)"
+    "rhs_url": "https://www.rhs.org.uk/plants/14184/pulsatilla-vulgaris/details",
+    "back_text": "Pulsatilla vulgaris\n\n(pasqueflower)"
   },
   {
-    "id": "card_49",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo68052.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa15932.jpg",
-    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa11442.jpg",
-    "rhs_url": "https://www.rhs.org.uk/plants/225/acer-palmatum/details",
-    "back_text": "Acer palmatum\n\n(Japanese maple)"
+    "id": "card_75",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-RHS_April_2018_101_814.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHAB18014_814.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHAB18013_814.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/7403/fritillaria-meleagris/details",
+    "back_text": "Fritillaria meleagris\n\n(snake’s head fritillary)"
   },
   {
-    "id": "card_50",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008730_49.JPG",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem12830.jpg",
+    "id": "card_94",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_MAR0015575_1527.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_PUB0001565_1527.JPG",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/311/achillea-millefolium/details",
-    "back_text": "Achillea millefolium\n\n(common yarrow)"
+    "rhs_url": "https://www.rhs.org.uk/plants/13901/primula-vulgaris-%28pr-prim%29/details",
+    "back_text": "Primula vulgaris\n\n(primrose)"
   },
   {
-  "id": "card_51",
-  "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem03257.jpg",
-  "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0012675.jpg",
+    "id": "card_99",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026549_5929.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008933_4068.JPG",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/58880/rudbeckia-fulgida-var-deamii/details",
+    "back_text": "Rudbeckia fulgida var. deamii\n\n(Deam’s coneflower)"
+  },
+  {
+    "id": "card_100",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT1102_1760.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/TN_11_Salvia_nemorosa_'Ostfriesland'_TM_Trials_Field_(5).jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/phbe03185.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/78346/salvia-nemorosa-ostfriesland/details",
+    "back_text": "Salvia nemorosa 'Ostfriesland'\n\n(Balkan clary)"
+  },
+  {
+    "id": "card_102",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008642_1882.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT9473_1882.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/17802/stipa-gigantea/details",
+    "back_text": "Stipa gigantea\n\n(golden oats)"
+  },
+  {
+    "id": "card_103",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0012099_5374.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo47113.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/brer03557.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/340226/symphyotrichum-novi-belgii-fellowship-%28d%29/details",
+    "back_text": "Symphyotrichum novi-belgii 'Fellowship'\n\n(New York aster)"
+  },
+  {
+    "id": "card_109",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/MAR0003574_1993.jpg",
+    "front_image_url_2": "",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/42079/verbena-bonariensis/details",
+    "back_text": "Verbena bonariensis\n\n(purple top)"
+  },
+  {
+    "id": "card_85",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034554_8807.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/phbe04619.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/10920/matteuccia-struthiopteris/details",
+    "back_text": "Matteuccia struthiopteris\n\n(shuttlecock fern)"
+  }
+];  "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0012675.jpg",
   "front_image_url_3": "",
   "rhs_url": "https://www.rhs.org.uk/plants/24962/aegopodium-podagraria/details",
   "back_text": "Aegopodium podagraria\n\n(ground elder)"
