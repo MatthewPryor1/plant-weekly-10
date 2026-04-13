44908609b149ced3047127334ae93812add3a1a5
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-04-13 13:19:43 +0100

    Update front image URLs for several cards

--- cards.js
+++ cards.js
@@ -10,8 +10,8 @@ const cards = [
   {
     "id": "card_96",
     "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHDE18096_1572.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa16092.jpg",
-    "front_image_url_3": "",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHDE18097_1572.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa16092.jpg",
     "rhs_url": "https://www.rhs.org.uk/plants/14184/pulsatilla-vulgaris/details",
     "back_text": "Pulsatilla vulgaris\n\n(pasqueflower)"
   },
@@ -35,7 +35,7 @@ const cards = [
     "id": "card_99",
     "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026549_5929.jpg",
     "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008933_4068.JPG",
-    "front_image_url_3": "",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/visi11997.jpg",
     "rhs_url": "https://www.rhs.org.uk/plants/58880/rudbeckia-fulgida-var-deamii/details",
     "back_text": "Rudbeckia fulgida var. deamii\n\n(Deam’s coneflower)"
   },
@@ -51,7 +51,7 @@ const cards = [
     "id": "card_102",
     "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008642_1882.JPG",
     "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT9473_1882.jpg",
-    "front_image_url_3": "",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_HYDE_HALL_067.jpg",
     "rhs_url": "https://www.rhs.org.uk/plants/17802/stipa-gigantea/details",
     "back_text": "Stipa gigantea\n\n(golden oats)"
   },
@@ -66,7 +66,7 @@ const cards = [
   {
     "id": "card_109",
     "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/MAR0003574_1993.jpg",
-    "front_image_url_2": "",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0020807_1993.jpg",
     "front_image_url_3": "",
     "rhs_url": "https://www.rhs.org.uk/plants/42079/verbena-bonariensis/details",
     "back_text": "Verbena bonariensis\n\n(purple top)"
