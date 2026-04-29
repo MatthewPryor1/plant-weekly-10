bd4d862883e98d97d9254cfa963587501e7e34b1
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-04-29 17:50:43 +0100

    Update card entries with new plant data

--- cards.js
+++ cards.js
@@ -1,98 +1,156 @@
 const cards = [
   {
-    "id": "card_51",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem03257.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0012675.jpg",
+    "id": "card_49",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo68052.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa15932.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa11442.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/384/acer-palmatum/details",
+    "back_text": "Acer palmatum\n\n(Japanese maple)\n\nacer (Latin) = hard / sharp\npalmatum (Latin) = like a palm / hand\n(palmate leaves)"
+  },
+  {
+    "id": "card_76",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035798_3951.jpg",
+    "front_image_url_2": "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-leaf.jpg?itok=bFc72-Q2",
+    "front_image_url_3": "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-axial-buds.jpg?itok=uav9xOPU",
+    "rhs_url": "https://www.rhs.org.uk/plants/7990/ginkgo-biloba/details",
+    "back_text": "Ginkgo biloba\n\n(maidenhair tree)\n\nyin (Chinese) = silver\nduo = apricot\nJapanese pronunciation = ginko\nbiloba (Latin) = two-lobed"
+  },
+  {
+    "id": "card_86",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026463_3935.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0017876_1276.JPG",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0010102_1276.JPG",
+    "rhs_url": "https://www.rhs.org.uk/plants/11093/metasequoia-glyptostroboides/details",
+    "back_text": "Metasequoia glyptostroboides\n\n(dawn redwood)\n\nmeta (Greek) = with / sharing\nsequoia = which see\n(redwood genus)\nglyptostrobo (Latin) = resemble Glyptostrobus\n(a Chinese swamp cypress)\n-oides (La) = similar to"
+  },
+  {
+    "id": "card_57",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_PUB0000254_10610.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/rera04968.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/24962/aegopodium-podagraria/details",
-    "back_text": "Aegopodium podagraria\n\n(ground elder)\n\naka goatweed\n\naegos (Greek) = goat\npodi (Gr) = foot\n\"little goat's foot\"\npoda/i (English) = foot\npodagia = foot gout\nagraria (Latin) = cultivated land\n\nGood for goats & good for gout!"
+    "rhs_url": "https://www.rhs.org.uk/plants/2448/buddleja-davidii/details",
+    "back_text": "Buddleja davidii\n\n(butterfly bush)\n\nAfter Reverend Buddle (English botanist)\ndavidii = after Père Armand David\n(French missionary and plant collector in China)"
   },
   {
-    "id": "card_55",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0036008_3483.jpg",
-    "front_image_url_2": "",
+    "id": "card_65",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035788_4153.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa02344.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa02276.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/4511/corylus-avellana-%28f%29/details",
+    "back_text": "Corylus avellana\n\n(hazel)\n\nkorys (Greek) = hood / helmet\n(calyx covering the nut)\navellana = after Avella near Naples\nwhere hazel is grown for its nuts"
+  },
+  {
+    "id": "card_67",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0005133_10215.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem02042.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/94326/bellis-perennis/details",
-    "back_text": "Bellis perennis\n\n(daisy)\n\nBellis (Latin) = pretty / beautiful\nperennis (La): per = through, annus = year\n= everlasting perennial"
+    "rhs_url": "https://www.rhs.org.uk/plants/4552/cotinus-coggygria/details",
+    "back_text": "Cotinus coggygria\n\n(smoke tree)\n\ncotinus (Greek) = wild olive\n(has olive-like appearance)\ncoggygria (Greek) = incorrect interpretation of 'kokkugia'\n= old name for smoke tree\n= smoky feathery plumes"
+  },
+  const cards = [
+  {
+    "id": "card_49",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo68052.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa15932.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa11442.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/384/acer-palmatum/details",
+    "back_text": "Acer palmatum\n\n(Japanese maple)\n\nacer (Latin) = hard / sharp\npalmatum (Latin) = like a palm / hand\n(palmate leaves)"
   },
   {
-    "id": "card_58",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-MAR0015606_295.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0015827_3628.jpg",
-    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo68770.jpg",
-    "rhs_url": "https://www.rhs.org.uk/plants/2803/caltha-palustris/details",
-    "back_text": "Caltha palustris\n\n(marsh marigold)\n\nCaltha (Greek) = goblet\npalustris (Latin) = found in marshy places"
+    "id": "card_76",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035798_3951.jpg",
+    "front_image_url_2": "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-leaf.jpg?itok=bFc72-Q2",
+    "front_image_url_3": "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-axial-buds.jpg?itok=uav9xOPU",
+    "rhs_url": "https://www.rhs.org.uk/plants/7990/ginkgo-biloba/details",
+    "back_text": "Ginkgo biloba\n\n(maidenhair tree)\n\nyin (Chinese) = silver\nduo = apricot\nJapanese pronunciation = ginko\nbiloba (Latin) = two-lobed"
   },
   {
-    "id": "card_59",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS-0005209.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/PUB0038602.jpg",
-    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/PUB0038603.jpg",
-    "rhs_url": "https://www.rhs.org.uk/plants/20686/calystegia-sepium/details",
-    "back_text": "Calystegia sepium\n\n(hedge bindweed)\n\ncalyx (Greek) = cup / sepals\nstegia (Gr) = covered roof\nie large bracts from calyx\nsepium (Latin) = hedges / fences"
+    "id": "card_86",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026463_3935.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0017876_1276.JPG",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0010102_1276.JPG",
+    "rhs_url": "https://www.rhs.org.uk/plants/11093/metasequoia-glyptostroboides/details",
+    "back_text": "Metasequoia glyptostroboides\n\n(dawn redwood)\n\nmeta (Greek) = with / sharing\nsequoia = which see\n(redwood genus)\nglyptostrobo (Latin) = resemble Glyptostrobus\n(a Chinese swamp cypress)\n-oides (La) = similar to"
   },
   {
-    "id": "card_62",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0015991_11873.JPG",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0014517_11873.JPG",
+    "id": "card_57",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_PUB0000254_10610.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/rera04968.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/105534/capsella-bursa-pastoris/details",
-    "back_text": "Capsella bursa-pastoris\n\n(shepherd’s purse)\n\ncapsa (Latin) = case / box / chest\n-ella = little / small\nbursa (La / Greek) = pouch / purse\npastoris (La) = pastor / shepherd\n\n\"Little shepherd's purse\""
+    "rhs_url": "https://www.rhs.org.uk/plants/2448/buddleja-davidii/details",
+    "back_text": "Buddleja davidii\n\n(butterfly bush)\n\nAfter Reverend Buddle (English botanist)\ndavidii = after Père Armand David\n(French missionary and plant collector in China)"
   },
   {
-    "id": "card_82",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0007338_3712.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi71408.jpg",
-    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS-0004025.jpg",
-    "rhs_url": "https://www.rhs.org.uk/plants/10394/lobelia-cardinalis/details",
-    "back_text": "Lobelia cardinalis\n\n(cardinal flower)\n\nAfter Flemish botanist Matthias de L'Obel (1538–1616)\nphysician to James I / VI\ncardinalis (Latin) = scarlet / cardinal red"
+    "id": "card_65",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035788_4153.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa02344.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa02276.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/4511/corylus-avellana-%28f%29/details",
+    "back_text": "Corylus avellana\n\n(hazel)\n\nkorys (Greek) = hood / helmet\n(calyx covering the nut)\navellana = after Avella near Naples\nwhere hazel is grown for its nuts"
   },
   {
-    "id": "card_93",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034360_3354.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo02814.jpg",
+    "id": "card_67",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0005133_10215.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem02042.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/13502/pontederia-cordata/details",
-    "back_text": "Pontederia cordata\n\n(pickerel weed)\n\nAfter J Pontederia, botanist at Padua\ncordata (Latin) = heart-shaped\n(leaves)\n\nHardy aquatic"
+    "rhs_url": "https://www.rhs.org.uk/plants/4552/cotinus-coggygria/details",
+    "back_text": "Cotinus coggygria\n\n(smoke tree)\n\ncotinus (Greek) = wild olive\n(has olive-like appearance)\ncoggygria (Greek) = incorrect interpretation of 'kokkugia'\n= old name for smoke tree\n= smoky feathery plumes"
+  },
+    {
+    "id": "card_60",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/JFCampsisxtagliabuanaIndianSummer1.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/rera02511.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/visi102101.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/331252/campsis-tagliabueana-indian-summer-(-kudian-pbr)/details",
+    "back_text": "Campsis radicans 'Indian Summer'\n\n(trumpet vine)\n\nkampsis (Greek) = curving / bending\n(curved stamens)\nradicans (Latin) = trailing or rooting\n(adventitious roots for climbing)\n'Indian Summer' = blooming time + colour\n(late summer / early autumn)"
+  }
+  {
+    "id": "card_83",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0010184_3895.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0018145_1191.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0018144_1191.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/74793/lonicera-periclymenum-graham-thomas/details",
+    "back_text": "Lonicera periclymenum 'Graham Thomas'\n\n(common honeysuckle)\n\nLonicera = after Adam Lonicer\n(German botanist)\npericlymenum (Greek) = woodbine / honeysuckle\nPericlymenus / Periklymenon = an Argonaut who could change shape, form, colour\nperikleia (Gr) = I entwine / to entwine around\n(entwining habit)"
   },
   {
-    "id": "card_97",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0016065_8367.JPG",
-    "front_image_url_2": "",
+    "id": "card_90",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035917_3890.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi08159.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/14373/ranunculus-repens/details",
-    "back_text": "Ranunculus repens\n\n(creeping buttercup)\n\nrana (Latin) = frog\n(likes damp areas)\n-culus = little\nrepens (La) = creeping\n\n\"Creeping little frog\""
+    "rhs_url": "https://www.rhs.org.uk/plants/12349/parthenocissus-quinquefolia/details",
+    "back_text": "Parthenocissus quinquefolia\n\n(Virginia creeper)\n\nparthenos (Greek) = virgin / maiden\n(can produce seeds without pollination)\n-cissus = ivy\nquinquefolia (Latin) = five-leaved\ncompound palmate leaves\n'five-leaved virgin ivy'"
   },
   {
-    "id": "card_101",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0012466_11855.JPG",
-    "front_image_url_2": "",
+    "id": "card_110",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0040456_12943.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT9073_2045.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/moja07052.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/19126/wisteria-sinensis/details",
+    "back_text": "Wisteria sinensis\n\n(Chinese wisteria)\n\nWisteria = after Caspar Wistar\n(Professor at the University of Pennsylvania)\nsinensis = Chinese\n(anticlockwise twining stem - to left)"
+  }
+];
+  {
+    "id": "card_83",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0010184_3895.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0018145_1191.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0018144_1191.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/74793/lonicera-periclymenum-graham-thomas/details",
+    "back_text": "Lonicera periclymenum 'Graham Thomas'\n\n(common honeysuckle)\n\nLonicera = after Adam Lonicer\n(German botanist)\npericlymenum (Greek) = woodbine / honeysuckle\nPericlymenus / Periklymenon = an Argonaut who could change shape, form, colour\nperikleia (Gr) = I entwine / to entwine around\n(entwining habit)"
+  },
+  {
+    "id": "card_90",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035917_3890.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi08159.jpg",
     "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/119111/stellaria-media/details",
-    "back_text": "Stellaria media\n\n(common chickweed)\n\nStellaria (Latin) = star\nmedia (La) = intermediate / medium-sized"
+    "rhs_url": "https://www.rhs.org.uk/plants/12349/parthenocissus-quinquefolia/details",
+    "back_text": "Parthenocissus quinquefolia\n\n(Virginia creeper)\n\nparthenos (Greek) = virgin / maiden\n(can produce seeds without pollination)\n-cissus = ivy\nquinquefolia (Latin) = five-leaved\ncompound palmate leaves\n'five-leaved virgin ivy'"
   },
   {
-    "id": "card_105",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/eyma01394.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem09113.jpg",
-    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa11920.jpg",
-    "rhs_url": "https://www.rhs.org.uk/plants/232327/taraxacum-officinale/details",
-    "back_text": "Taraxacum officinale\n\n(dandelion)\n\ntaraxis (Greek / Persian) = bitter / disorder\nakas (Gr) = remedy\nofficina (Latin) = workshop / pharmacy / monastic storeroom\n\nHistoric medieval use"
-  },
-  {
-    "id": "card_107",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035821_4099.jpg",
-    "front_image_url_2": "",
-    "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/18565/typha-angustifolia/details",
-    "back_text": "Typha angustifolia\n\n(lesser bulrush)\n\nTypha (Greek) = cat's tail\n(Ti-fa)\nangustifolia (Latin) = narrow-leaved"
-  },
-  {
-    "id": "card_108",
-    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/sepa04809.jpg",
-    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0011544.jpg",
-    "front_image_url_3": "",
-    "rhs_url": "https://www.rhs.org.uk/plants/18618/urtica-dioica/details",
-    "back_text": "Urtica dioica\n\n(common stinging nettle)\n\nUrtica (Latin) = to burn\ndioica (Greek)\ndi = two\noikia = houses\n\n(as dioecious - plant reproduction)"
+    "id": "card_110",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0040456_12943.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT9073_2045.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/moja07052.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/19126/wisteria-sinensis/details",
+    "back_text": "Wisteria sinensis\n\n(Chinese wisteria)\n\nWisteria = after Caspar Wistar\n(Professor at the University of Pennsylvania)\nsinensis = Chinese\n(anticlockwise twining stem - to left)"
   }
 ];
